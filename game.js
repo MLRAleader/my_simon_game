1bf5c2b0d37c7331339c0974a4847eb410bf78f7
Axel <44190680+MLRAleader@users.noreply.github.com>
2024-02-20 23:34:10 +0100

    Adding file

---
+++ game.js
@@ -0,0 +1,104 @@
+var tabColour = ["green", "red", "yellow", "blue"];
+var tabPossibleUserChoiceColour = $(".btn");
+var level = 0;
+var tabSelectedRandomColour = [];
+var tabUserSelectedColour = [];
+var gameStarted = false;
+gameStarter();
+function gameStarter() {
+  if (!gameStarted) {
+    gameStarted = true;
+    $(document).one("keypress", function () {
+      initGame();
+      gameStarted = false;
+    });
+  }
+}
+function initGame() {
+  initScoreToZero();
+  generateAndExtendSequence();
+  userSelectingColour();
+  
+}
+
+function generateRandomColour() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return tabColour[randomNumber];
+}
+
+function selectRandomColour(randomColour) {
+  return $("#" + randomColour);
+}
+
+function changeDisplayEffectRandomSelectedColour(selectedRandomColourId) {
+  $("#" + selectedRandomColourId).addClass("pressed");
+  setTimeout(function () {
+    $("#" + selectedRandomColourId).removeClass("pressed");
+  }, 100);
+}
+
+function playColourSound(selectedRandomColourId) {
+  var selectedColourSound = new Audio("/sounds/" + selectedRandomColourId + ".mp3");
+  selectedColourSound.play();
+}
+
+function userPlaying(event) {
+  var selectedRandomColourId = tabSelectedRandomColour[tabUserSelectedColour.length];
+  var userSelectedColourId = $(event.target).attr("id");
+
+  if (userSelectedColourId === selectedRandomColourId) {
+    tabUserSelectedColour.push(userSelectedColourId);
+    changeDisplayEffectRandomSelectedColour(userSelectedColourId);
+    playColourSound(userSelectedColourId);
+    if (tabUserSelectedColour.length === tabSelectedRandomColour.length) {
+      level++;
+      changeScore();
+      setTimeout(generateAndExtendSequence, 800);
+    }
+  } else {
+    displayMessage("Game Over, Press Any Key to Restart");
+    resetGame();
+  }
+}
+
+function userSelectingColour() {
+  tabPossibleUserChoiceColour.off("click"); 
+  tabPossibleUserChoiceColour.on("click", userPlaying);
+}
+
+function generateAndExtendSequence() {
+  var newRandomColour = generateRandomColour();
+  tabSelectedRandomColour.push(newRandomColour);
+  displayMessage("Level " + level);
+  setTimeout(function () {
+    for (var i = 0; i < tabSelectedRandomColour.length; i++) {
+      (function (i) {
+        setTimeout(function () {
+          var colorId = tabSelectedRandomColour[i];
+          changeDisplayEffectRandomSelectedColour(colorId);
+          playColourSound(colorId);
+        }, i * 900);
+      })(i);
+    }
+    tabUserSelectedColour = [];
+  }, 1000);
+}
+
+function displayMessage(message) {
+  $("#level-title").text(message);
+}
+
+function changeScore() {
+  $("#level-title").text("Level " + level);
+}
+
+function initScoreToZero() {
+  level = 0;
+  changeScore();
+}
+
+function resetGame() {
+  tabSelectedRandomColour = [];
+  tabUserSelectedColour = [];
+  gameStarter();
+}
